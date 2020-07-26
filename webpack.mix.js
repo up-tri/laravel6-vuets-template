df903bb9249f50c8d68d0f51a340b473b2e11c59
Kohei Seta <ktarou78@gmail.com>
2020-07-26 13:48:16 +0900

    linter etc.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,75 @@
 const mix = require('laravel-mix');
+const WatchTimePlugin = require('webpack-watch-time-plugin');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.webpackConfig({
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }, {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: "babel-loader",
+          options: {
+            babelrc: false,
+          }
+        },
+        {
+          loader: "ts-loader",
+          options: {
+            configFile: "tsconfig.json",
+            appendTsSuffixTo: [/\.vue$/]
+          }
+        },
+        ]
+      }, {
+        test: /\.js?$/,
+        use: [{
+          loader: 'babel-loader',
+          options: mix.config.babel()
+        }]
+      }, {
+        test: /\.scss/,
+        enforce: "pre",
+        loader: 'import-glob-loader'
+      }],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+  },
+  plugins: [
+    new WatchTimePlugin()
+  ]
+});
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.ts("./resources/ts/app.ts", "./public/js");
+mix.sass('./resources/sass/app.scss', './public/css');
+mix.copy("./resources/img/**/*", "./public/images/");
+mix.browserSync({
+  proxy: {
+    target: "localhost",
+    ws: true
+  },
+  files: [
+    './public/css/*.css',
+    './public/js/*.js',
+    './app/**/*',
+    './config/**/*',
+    './resources/views/**/*.blade.php',
+    './routes/**/*'
+  ],
+  https: false,
+  host: "localhost",
+  port: 3000
+});
+
+if (mix.inProduction()) {
+  mix.version();
+  mix.disableNotifications();
+}
